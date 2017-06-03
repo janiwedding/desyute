b79a879c25a84b31789e733faf097f24118305e3
iamdesu <made.sudarsana@mitrais.com>
2017-06-03 22:11:35 +0800

    Initialization

---
+++ js/script.js
@@ -0,0 +1,64 @@
+﻿(function($){
+	"use strict";
+
+	$(document).ready(function() {
+		"use strict";
+
+		/*ScrollR */
+		if ($(window).width() > 1280) {
+			var iniiSkrollr = skrollr.init({
+				forceHeight: false
+			});
+		}
+
+		/*Main Menu Button */
+		$('.main_menu_btn').on("click", function(e){
+			$(this).toggleClass('main_menu_btn_open');
+			$('.main_menu_block').toggleClass('main_menu_block_open').fadeToggle();
+			$('.main_menu_block').find('.menu_wrapper').toggleClass('active');
+			$('header .anim').toggleClass('active');
+			e.preventDefault();
+		});
+		
+		/* Section Background */
+		$('section, .parallax').each(function(){
+			var image = $(this).attr('data-image');
+			if (image){
+				$(this).css('background-image', 'url('+image+')');	
+			}
+		});
+		
+		/*Scroll Effect*/
+		$('.scrollTo').on("click", function(e){
+			var anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 1000);
+			e.preventDefault();
+		});
+
+		$('.image-popup-link').magnificPopup({
+			type:'image',
+			mainClass: 'mfp-with-zoom',
+			zoom: {
+				enabled: true, // By default it's false, so don't forget to enable it
+
+				duration: 300, // duration of the effect, in milliseconds
+				easing: 'ease-in-out', // CSS transition easing function
+
+				// The "opener" function should return the element from which popup will be zoomed in
+				// and to which popup will be scaled down
+				// By defailt it looks for an image tag:
+				opener: function(openerElement) {
+					// openerElement is the element on which popup was initialized, in this case its <a> tag
+					// you don't need to add "opener" option if this code matches your needs, it's defailt one.
+					return openerElement.is('img') ? openerElement : openerElement.find('img');
+				}
+			},
+			gallery: {
+				enabled:true
+			}
+		});
+
+	});
+})(jQuery);
